44e7c0f5007ae3b16da3dfd08bc3d27ebaa0f271
Şirin Ekşi <sirin.eksi@hotmail.com>
2023-05-22 13:51:23 +0300

    Initial Commit

---
+++ PreviewSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PreviewSwiftUI
+//
+//  Created by ceksi on 22.05.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PreviewSwiftUI/PreviewSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PreviewSwiftUIApp.swift
+//  PreviewSwiftUI
+//
+//  Created by ceksi on 22.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct PreviewSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
